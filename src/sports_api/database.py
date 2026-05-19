63bc96622d2bf1cb5d70b82f4df184bf29b035ca
Abel Castro <hi@abelcastro.dev>
2026-05-19 22:39:31 +0200

    Fix db connection errors

--- src/sports_api/database.py
+++ src/sports_api/database.py
@@ -2,7 +2,11 @@ from sqlmodel import Session, create_engine
 
 from sports_api.config import settings
 
-engine = create_engine(settings.database_url)
+engine = create_engine(
+    settings.database_url,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+)
 
 
 def get_session():
